19bcca1cb7c3c9576338681e2b33a94110f44b2e
EsioFreitas <esio.gustavo@gmail.com>
2019-03-11 19:13:22 -0300

    Add bars btn

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@
     var $body = document.querySelector('body');
     $body.classList.remove('no-js');
     $body.classList.add('js')
+
+    $btnMenu = document.querySelector('.main-header__btn');
+    $btnMenu.removeAttribute('style');
 })()
